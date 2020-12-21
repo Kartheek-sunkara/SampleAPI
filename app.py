7585a5a0f903130a40a177a43e7c0df894222a79
Kartheek-sunkara <69846401+Kartheek-sunkara@users.noreply.github.com>
2020-12-21 22:04:16 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,38 @@
+from flask import Flask,abort,render_template,request,redirect,url_for,jsonify
+import os
+import spacy
+import tabula
+from flashtext import KeywordProcessor
+import tika
+keyword_processor = KeywordProcessor()
+from tika import parser
+import xml.etree.ElementTree as ET
+import requests
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import base64
+import string
+import re
+from collections import Counter
+from nltk.corpus import stopwords
+import nltk
+nltk.download('stopwords')
+stopwords = stopwords.words('english')
+from sklearn.model_selection import train_test_split
+
+app = Flask(__name__)
+
+@app.route('/app2')
+def startApp():
+    return "i am from example flask"
+
+@app.route('/classification',methods = ['POST', 'GET'])
+def classification():  
+    #result=request.args.get('pdf_path')
+    return "Done"
+    
+    
+if __name__ == "__main__":
+     app.debug = True
+     app.run(port=5000)
